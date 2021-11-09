437058eb863a11c4339c4618a043f342d1d1adb9
Filippos <cs_fkat@outlook.com.gr>
2021-11-09 19:27:05 +0200

    Initial commit with layout changes

---
+++ app/src/main/java/com/example/calc/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.calc.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.calc.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/calc/presentation/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.calc.presentation
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+}
