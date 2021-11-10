3c9702df2a165e07da12529ae0c1447f0953325e
Filippos <cs_fkat@outlook.com.gr>
2021-11-10 15:40:12 +0200

    Add basic click listeners for buttons

--- app/src/main/java/com/example/calc/presentation/MainActivity.kt
+++ app/src/main/java/com/example/calc/presentation/MainActivity.kt
@@ -1,12 +1,64 @@
 package com.example.calc.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.calc.R
+import androidx.appcompat.app.AppCompatActivity
+import com.example.calc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+        initializeButtons()
     }
+
+    private fun initializeButtons() {
+        with(mainBinding) {
+            backspaceBtn.setOnClickListener {
+                val text = numberInputField.text
+                if (!text.isNullOrEmpty()) {
+                    val updatedText = text.toString().trim()
+                    val tedText = updatedText.substring(0, updatedText.length - 1)
+                    numberInputField.setText(tedText)
+                    numberInputField.setSelection(tedText.length)
+                }
+            }
+
+            oneBtn.setOnClickListener {
+                numberInputField.append("1")
+            }
+            twoBtn.setOnClickListener {
+                numberInputField.append("2")
+            }
+            threeBtn.setOnClickListener {
+                numberInputField.append("3")
+            }
+            fourBtn.setOnClickListener {
+                numberInputField.append("4")
+            }
+            fiveBtn.setOnClickListener {
+                numberInputField.append("5")
+            }
+            sixBtn.setOnClickListener {
+                numberInputField.append("6")
+            }
+            sevenBtn.setOnClickListener {
+                numberInputField.append("7")
+            }
+            eightBtn.setOnClickListener {
+                numberInputField.append("8")
+            }
+            nineBtn.setOnClickListener {
+                numberInputField.append("9")
+            }
+            acBtn.setOnClickListener {
+                numberInputField.text?.clear()
+            }
+        }
+
+    }
+
 }
