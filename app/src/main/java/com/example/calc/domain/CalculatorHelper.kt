03227106cf89fbdd1f033bcb8042731896c3e3b7
Filippos <cs_fkat@outlook.com.gr>
2021-12-27 23:02:33 +0200

    Work on operations with brackets

--- app/src/main/java/com/example/calc/domain/CalculatorHelper.kt
+++ app/src/main/java/com/example/calc/domain/CalculatorHelper.kt
@@ -23,7 +23,8 @@ interface CalculatorHelper {
     fun isPlainNumber(textInput: String): Boolean
     fun reverseNumber(): String
     fun addParenthesis(cursorPosition: Int): String
-    fun isNegative(textInput: String) : Boolean
+    fun isNegative(textInput: String): Boolean
+    fun isEquationCorrect(equation: String): Boolean
 }
 
 //By using the @inject constructor we tell hilt how he can provide us an Calculator instance
@@ -96,9 +97,6 @@ class Calculator @Inject constructor() : CalculatorHelper {
                     return textResult
                 }
                 Operation.DIVISION -> {
-                    if (denotesPlusOrMinusSign(textInput, Operation.ADDITION.operatorSymbol)) {
-                        return Operation.SIGNED_NUMBER_REPRESENTATION.name
-                    }
                     if (isEquationNotComplete(textInput, Operation.ADDITION.operatorSymbol)) {
                         return Operation.INCOMPLETE.name
                     }
@@ -111,9 +109,6 @@ class Calculator @Inject constructor() : CalculatorHelper {
                     return textResult
                 }
                 Operation.MULTIPLICATION -> {
-                    if (denotesPlusOrMinusSign(textInput, Operation.ADDITION.operatorSymbol)) {
-                        return Operation.SIGNED_NUMBER_REPRESENTATION.name
-                    }
                     if (isEquationNotComplete(textInput, Operation.ADDITION.operatorSymbol)) {
                         return Operation.INCOMPLETE.name
                     }
@@ -132,12 +127,37 @@ class Calculator @Inject constructor() : CalculatorHelper {
     }
 
     private fun getNumbersFromOperation(
-        mathematicalOperation: String,
+        equation: String,
         operation: Char
     ): Pair<Double, Double> {
-        val firstNumber = mathematicalOperation.substringBefore(operation).toDouble()
-        val secondNumber = mathematicalOperation.substringAfter(operation).toDouble()
-        return firstNumber to secondNumber
+        return if (equation[0] == operation) {
+            val firstNumber = equation
+                .substring(1, equation.length)
+                .substringBefore(operation)
+                .removeParenthesisIfAvailable()
+                .addCharAtIndex(equation[0], 0)
+                .toDouble()
+            val secondNumber = equation
+                .substring(1, equation.length)
+                .substringAfter(operation)
+                .removeParenthesisIfAvailable()
+                .addCharAtIndex(equation[0], 0)
+                .toDouble()
+
+            firstNumber to secondNumber
+        } else {
+            val firstNumber = equation
+                .substringBefore(operation)
+                .removeParenthesisIfAvailable()
+                .toDouble()
+            val secondNumber = equation
+                .substringAfter(operation)
+                .removeParenthesisIfAvailable()
+                .toDouble()
+
+            firstNumber to secondNumber
+
+        }
     }
 
     override fun modifyDisplayedResult(number: Double): String {
@@ -148,7 +168,9 @@ class Calculator @Inject constructor() : CalculatorHelper {
     override fun isRootingPossible(): Boolean {
         val operator = findOperationOut()
         return (operator != Operation.INVALID
-                && !isEquationNotComplete(textInput, operator.operatorSymbol) && !isNegative(textInput))
+                && !isEquationNotComplete(textInput, operator.operatorSymbol) && !isNegative(
+            textInput
+        ))
                 || operator == Operation.INVALID
     }
 
@@ -172,20 +194,28 @@ class Calculator @Inject constructor() : CalculatorHelper {
         val numberAfterOperator = textInput.substringAfter(textInput[operatorPosition])
         val numberBeforeOperator = textInput.substringBefore(textInput[operatorPosition])
         return if (cursorPosition > operatorPosition) {
-            numberBeforeOperator + applyReversalPattern(numberAfterOperator)
+            numberBeforeOperator + operator.operatorSymbol + applyReversalPattern(
+                numberAfterOperator
+            )
         } else {
-            applyReversalPattern(numberBeforeOperator) + numberAfterOperator
+            applyReversalPattern(numberBeforeOperator) + operator.operatorSymbol + numberAfterOperator
         }
     }
 
     private fun applyReversalPattern(numberToReverse: String): String {
-        return if (numberToReverse.contains("(") || numberToReverse.contains(")")) {
-            val resultWithoutParenthesis = numberToReverse.removePrefix("(").removeSuffix(")")
+        return if (numberToReverse.contains(OPENING_PARENTHESIS) ||
+            numberToReverse.contains(CLOSING_PARENTHESIS)
+        ) {
+            val resultWithoutParenthesis = numberToReverse.removeParenthesisIfAvailable()
             reversalPattern(resultWithoutParenthesis)
         } else {
             val reversedNumber = reversalPattern(numberToReverse)
             "($reversedNumber)"
         }
+    }
+
+    private fun String.removeParenthesisIfAvailable(): String {
+        return removePrefix(OPENING_PARENTHESIS).removeSuffix(CLOSING_PARENTHESIS)
     }
 
     private fun reversalPattern(numberToReverse: String): String {
@@ -207,19 +237,19 @@ class Calculator @Inject constructor() : CalculatorHelper {
         return containsAlreadyOperator
     }
 
-    private fun isEquationCorrect(equation: String): Boolean =
+    override fun isEquationCorrect(equation: String): Boolean =
         MATHEMATICAL_EXPRESSION_VALIDATOR.toRegex().matches(equation)
 
 
-    private fun denotesPlusOrMinusSign(mathematicalOperation: String, operator: Char): Boolean {
+    private fun denotesPlusOrMinusSign(equation: String, operator: Char): Boolean {
         var numberOfOperators = 0
-        mathematicalOperation.forEach {
+        equation.forEach {
             if (it == Operation.ADDITION.operatorSymbol || it == Operation.SUBTRACTION.operatorSymbol) numberOfOperators += 1
         }
         return if (numberOfOperators > 1) {
             false
         } else {
-            mathematicalOperation.substringBefore(operator).isEmpty()
+            equation.substringBefore(operator).isEmpty()
         }
     }
 
@@ -233,10 +263,10 @@ class Calculator @Inject constructor() : CalculatorHelper {
 
     private fun findOperationOut(): Operation {
         return when {
+            textInput.contains("×") -> Operation.MULTIPLICATION
+            textInput.contains("/") -> Operation.DIVISION
             textInput.contains("+") -> Operation.ADDITION
             textInput.contains("-") -> Operation.SUBTRACTION
-            textInput.contains("/") -> Operation.DIVISION
-            textInput.contains("×") -> Operation.MULTIPLICATION
             else -> Operation.INVALID
         }
     }
@@ -247,11 +277,16 @@ class Calculator @Inject constructor() : CalculatorHelper {
         return decimalPart > 0.0000
     }
 
+    private fun String.addCharAtIndex(char: Char, index: Int) =
+        StringBuilder(this).apply { insert(index, char) }.toString()
+
     companion object {
         private const val DECIMAL_FORMAT = "#.################"
         private const val MATHEMATICAL_EXPRESSION_VALIDATOR = "^([-+/×]*\\d+(\\.\\d+)?)*"
         private const val NUMBER_VALIDATOR = "\\d+"
         private const val REVERSE_ATTRIBUTE = -1
+        private const val OPENING_PARENTHESIS = "("
+        private const val CLOSING_PARENTHESIS = ")"
     }
 
 }
