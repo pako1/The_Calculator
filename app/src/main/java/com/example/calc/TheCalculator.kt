33cd111072e328839244e4924ac9a51652a54303
Filippos <cs_fkat@outlook.com.gr>
2022-01-30 23:26:30 +0200

    Change again paths

--- app/src/main/java/com/example/calc/TheCalculator.kt
+++ app/src/main/java/com/example/calc/TheCalculator.kt
@@ -2,17 +2,12 @@ package com.example.calc
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 import timber.log.Timber
 
 @HiltAndroidApp
 class TheCalculator : Application() {
     override fun onCreate() {
         super.onCreate()
-        GlobalScope.launch {  }
-
-
         initializeTimber()
     }
 
